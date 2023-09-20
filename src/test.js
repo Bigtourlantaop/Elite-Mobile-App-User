0fa33cf2597055fd129fa69df19a647126981a76
Wittawat <wittawat.pho@ku.th>
2023-09-20 12:11:03 +0700

    UpDate

--- src/test.js
+++
@@ -1,8 +0,0 @@
-
-
-fetch('http://localhost:8000/gen12datenext')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.error('Error:', error));
